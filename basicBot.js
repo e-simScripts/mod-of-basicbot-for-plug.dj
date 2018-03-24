9d75f7634f3638ee2c8dcffd0b3fc6807e184e66
e-simScripts <erne.ernestas@gmail.com>
2018-03-24 12:41:15 +0100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -252,7 +252,7 @@
         loggedInID: null,
         scriptLink: 'https://rawgit.com/e-simScripts/mod-of-basicbot-for-plug.dj/master/basicBot.js',
         cmdLink: 'http://git.io/245Ppg',
-        chatLink: 'https://rawgit.com/e-simScripts/mod-of-basicbot-for-plug.dj/master/lang/en.json',
+        chatLink: 'https://rawgit.com/e-simScripts/mod-of-basicbot-for-plug.dj/master/lang/lt.json',
         chat: null,
         loadChat: loadChat,
         retrieveSettings: retrieveSettings,
@@ -260,8 +260,8 @@
         settings: {
             botName: 'basicBot',
             language: 'english',
-            chatLink: 'https://rawgit.com/basicBot/source/master/lang/en.json',
-            scriptLink: 'https://rawgit.com/basicBot/source/master/basicBot.js',
+            chatLink: 'https://rawgit.com/e-simScripts/mod-of-basicbot-for-plug.dj/master/lang/lt.json',
+            scriptLink: 'https://rawgit.com/e-simScripts/mod-of-basicbot-for-plug.dj/master/basicBot.js',
             roomLock: false, // Requires an extension to re-load the script
             startupCap: 1, // 1-200
             startupVolume: 0, // 0-100
